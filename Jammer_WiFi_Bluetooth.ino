dde5ba92731e60d736e598cac26937b878877946
Oleg Skvortsov <highsoft.soi@gmail.com>
2025-09-29 19:48:28 -0700

    Simplified code, removed unnecessary graphics and function.

--- Jammer_WiFi_Bluetooth.ino
+++ Jammer_WiFi_Bluetooth.ino
@@ -4,8 +4,6 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-//#include <string>
-#include "images.h"
 #include <ezButton.h>
 
 #define SCREEN_WIDTH 128
@@ -19,56 +17,6 @@ ezButton button(3);
 const int wifiFrequencies[] = {2412, 2417, 2422, 2427, 2432, 2437, 2442, 2447, 2452, 2457, 2462};
 const char* modes[] = {"BLE & All 2.4 GHz", "Just Wi-Fi", "Waiting Idly :("};
 uint8_t attack_type = 2;
-
-void displayMessage(const char* line, uint8_t x = 55, uint8_t y = 22, const unsigned char* bitmap = helpy_menu_image) {
-  radio.powerDown();
-  SPI.end();
-  delay(10);
-
-  display.clearDisplay();
-  if (bitmap != nullptr) {
-    display.drawBitmap(0, 0, bitmap, 128, 64, WHITE);
-  }
-  display.setTextSize(1);
-  String text = String(line);
-  int16_t cursor_y = y;
-  int16_t maxWidth = 128 - x;
-  while (text.length() > 0) {
-    int16_t charCount = 0;
-    int16_t lineWidth = 0;
-    while (charCount < text.length() && lineWidth < maxWidth) {
-      charCount++;
-      lineWidth = 6 * charCount;
-    }
-    if (charCount < text.length()) {
-      int16_t lastSpace = text.substring(0, charCount).lastIndexOf(' ');
-      if (lastSpace > 0) {
-        charCount = lastSpace + 1;
-      }
-    }
-    display.setCursor(x, cursor_y);
-    display.println(text.substring(0, charCount));
-    text = text.substring(charCount);
-    cursor_y += 10;
-    if (cursor_y > 64) break;
-  }
-  display.display();
-
-  SPI.begin();
-  radio.powerUp();
-  delay(5);
-  radio.startConstCarrier(RF24_PA_MAX, i);
-}
-
-void addvertising() {
-  for (size_t i = 0; i < 3; i++) {
-    displayMessage("", 60, 22, helpy_big_image);
-    delay(310);
-    displayMessage("", 60, 22, nullptr);
-    delay(300);
-  }
-  displayMessage("Jammer got up. Click the button and discover all modes!", 65, 6);
-}
 
 void fullAttack() {
   for (size_t i = 0; i < 80; i++) {
@@ -110,10 +58,16 @@ void setup() {
     radio.setCRCLength(RF24_CRC_DISABLED);
     radio.printPrettyDetails();
     radio.startConstCarrier(RF24_PA_MAX, i);
-    addvertising();
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.println("Jammer got up. Click the button and discover all modes!");
+    display.display();
   } else {
     Serial.println("BLE Jammer couldn't be started!");
-    displayMessage("Jammer Error!");
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.println("Jammer Error!");
+    display.display();
   }
 
 }
@@ -122,7 +76,10 @@ void loop() {
   button.loop();
   if (button.isPressed()) {
     attack_type = (attack_type + 1) % 3;
-    displayMessage((String(modes[attack_type])+" Mode").c_str());
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.println(modes[attack_type]);
+    display.display();
   }
   switch (attack_type) {
     case 0:
